de5b2421b024912b6043c3f7cad181d07ed295f4
Joel C <joel@vorce.se>
2018-08-20 21:32:55 +0200

    Add html suffix to posts and pages (#7)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gonz.MixProject do
   def project do
     [
       app: :gonz,
-      version: "1.1.0",
+      version: "1.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
