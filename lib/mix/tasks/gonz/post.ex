22752ec8c20a3ad9126813ce62619f121e3b11bc
Joel C <joel@vorce.se>
2018-08-28 22:21:39 +0200

    Add message for incorrect number of arguments for gonz.post

--- lib/mix/tasks/gonz/post.ex
+++ lib/mix/tasks/gonz/post.ex
@@ -10,8 +10,13 @@ defmodule Mix.Tasks.Gonz.Post do
     case Gonz.Post.create(title) do
       :ok ->
         IO.puts("Created new post with title: \"#{title}\" in posts/")
+
       other ->
         IO.puts("Failed to create new post with title: \"#{title}\", reason: #{inspect(other)}")
     end
+  end
+
+  def run(_) do
+    IO.puts("This task expect one argument, the title of the post")
   end
 end
