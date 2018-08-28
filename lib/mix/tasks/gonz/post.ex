98acf35925b981e3ba963001f6d28b82dd1307e4
Joel Carlbark <joel.carlbark@meltwater.com>
2018-08-28 21:53:43 +0200

    Enclose title in quotes for gonz.post output

--- lib/mix/tasks/gonz/post.ex
+++ lib/mix/tasks/gonz/post.ex
@@ -9,9 +9,9 @@ defmodule Mix.Tasks.Gonz.Post do
   def run([title]) do
     case Gonz.Post.create(title) do
       :ok ->
-        IO.puts("Created new post with title: #{title} in posts/")
+        IO.puts("Created new post with title: \"#{title}\" in posts/")
       other ->
-        IO.puts("Failed to create new post with title: #{title}, reason: #{inspect(other)}")
+        IO.puts("Failed to create new post with title: \"#{title}\", reason: #{inspect(other)}")
     end
   end
 end
