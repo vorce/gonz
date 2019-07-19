0ba40df466d762974e955ab2a513fc556a3008c1
Joel C <joel@vorce.se>
2019-07-19 12:41:15 +0200

    Add categories to front matter

--- test/front_matter_test.exs
+++ test/front_matter_test.exs
@@ -7,13 +7,17 @@ defmodule Gonz.FrontMatterTest do
     test "string to elixir map" do
       created_at = "2018-08-09T17:51:32.905935Z"
       title = "Hellö there"
-      front_matter_string = "%{\n  title: \"#{title}\",\n  created_at: \"#{created_at}\",\n  id: 1\n}"
+      categories = "[:nav_item, \"foo\"]"
+
+      front_matter_string =
+        "%{\n  title: \"#{title}\",\n  created_at: \"#{created_at}\",\n  id: 1,\n categories: #{categories}}"
 
       assert FrontMatter.parse(front_matter_string) == %Gonz.Markdown.FrontMatter{
                created_at: created_at,
                description: "",
                nav_item: false,
-               title: title
+               title: title,
+               categories: [:nav_item, "foo"]
              }
     end
   end
